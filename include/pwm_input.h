5ac026fde011a7a0e22dbcb9e3faadfb0efb3e4a
maxgallup <maxgallup@pm.me>
2024-02-23 12:09:50 +0100

    fix: finally hardwware timers work

--- include/pwm_input.h
+++ include/pwm_input.h
@@ -3,26 +3,26 @@
 
 #include <SimpleFOC.h>
 
-#define pin  A_BUTTON
+#define pin A_PWM
 
 uint32_t channel_rising, channel_falling;
-volatile uint32_t frequency_measured, dutycycle_measured, last_period_capture = 0, current_capture, high_state_measured;
+volatile uint32_t frequency_measured, adjusted_duty, last_period_capture = 0, current_capture, high_state_measured;
 uint32_t input_freq = 0;
 volatile uint32_t rollover_compare_count = 0;
 HardwareTimer *timer;
 
+#define MAGIC_OFFSET 69 + 100
 
 void signal_rising_callback(void) {
-  Serial.println("rising callback");
   current_capture = timer->getCaptureCompare(channel_rising);
   /* frequency computation */
   if (current_capture > last_period_capture) {
-    frequency_measured = input_freq / (current_capture - last_period_capture);
-    dutycycle_measured = (high_state_measured * 100) / (current_capture - last_period_capture);
+    // frequency_measured = input_freq / (current_capture - last_period_capture);
+    adjusted_duty = (high_state_measured * 1600) / (current_capture - last_period_capture) - MAGIC_OFFSET;
   } else if (current_capture <= last_period_capture) {
     /* 0x1000 is max overflow value */
-    frequency_measured = input_freq / (0x10000 + current_capture - last_period_capture);
-    dutycycle_measured = (high_state_measured * 100) / (0x10000 + current_capture - last_period_capture);
+    // frequency_measured = input_freq / (0x10000 + current_capture - last_period_capture);
+    adjusted_duty = (high_state_measured * 1600) / (0x10000 + current_capture - last_period_capture) - MAGIC_OFFSET;
   }
 
   last_period_capture = current_capture;
@@ -35,8 +35,8 @@ void rollover_callback(void) {
   rollover_compare_count++;
 
   if (rollover_compare_count > 1) {
-    frequency_measured = 0;
-    dutycycle_measured = 0;
+    // frequency_measured = 0;
+    adjusted_duty = 0;
   }
 }
 
@@ -54,26 +54,10 @@ void signal_falling_callback(void) {
 }
 
 
-void test_callback(void) {
-
-  Serial.println("CHANGE!");
-  // Serial.println(timer->getCount());
-
-}
-
-
-
-
 
 void pwm_input_init() {
   pinMode(pin, INPUT);
 
-
-  // attachInterrupt(digitalPinToInterrupt(pin), test_callback, CHANGE);
-  // return;
-
-  // Automatically retrieve TIM instance and channel_rising associated to pin
-  // This is used to be compatible with all STM32 series automatically.
   TIM_TypeDef *Instance = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(pin), PinMap_PWM);
   channel_rising = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(pin), PinMap_PWM));
 
@@ -94,30 +78,17 @@ void pwm_input_init() {
       break;
   }
 
-  Serial.println(">>> channel_rising ");
-  Serial.println(channel_rising);
-  Serial.println(">>> channel_falling");
-  Serial.println(channel_falling);
-
-  // Instantiate HardwareTimer object. Thanks to 'new' instantiation, HardwareTimer is not destructed when setup() function is finished.
   timer = new HardwareTimer(Instance);
 
   // Configure rising edge detection to measure frequency
-  timer->setMode(channel_rising, TIMER_INPUT_CAPTURE_BOTHEDGE, pin);
+  timer->setMode(channel_rising, TIMER_INPUT_FREQ_DUTY_MEASUREMENT, pin);
 
-  // With a PrescalerFactor = 1, the minimum frequency value to measure is : TIM counter clock / CCR MAX
-  //  = (SystemCoreClock) / 65535
-  // Example on Nucleo_L476RG with systemClock at 80MHz, the minimum frequency is around 1,2 khz
-  // To reduce minimum frequency, it is possible to increase prescaler. But this is at a cost of precision.
-  // The maximum frequency depends on processing of both interruptions and thus depend on board used
-  // Example on Nucleo_L476RG with systemClock at 80MHz the interruptions processing is around 10 microseconds and thus Max frequency is around 100kHz
-  uint32_t PrescalerFactor = 1;
-  // timer->setPrescaleFactor(PrescalerFactor);
-  // timer->setOverflow(0x10000); // Max Period value to have the largest possible time to detect rising edge and avoid timer rollover
-  timer->attachInterrupt(channel_rising, test_callback);
-  // timer->attachInterrupt(channel_rising, signal_rising_callback);
-  // timer->attachInterrupt(channel_falling, signal_falling_callback);
-  // timer->attachInterrupt(rollover_callback);
+  uint32_t PrescalerFactor = 170;
+  timer->setPrescaleFactor(PrescalerFactor);
+  timer->setOverflow(0x10000); // Max Period value to have the largest possible time to detect rising edge and avoid timer rollover
+  timer->attachInterrupt(channel_rising, signal_rising_callback);
+  timer->attachInterrupt(channel_falling, signal_falling_callback);
+  timer->attachInterrupt(rollover_callback);
   
   timer->refresh();
   timer->resume();
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <SimpleFOC.h>
 #include "pwm_input.h"
-#include "pwm_naive.h"
-
 
 
 BLDCMotor motor = BLDCMotor(7);
@@ -11,11 +9,7 @@ LowsideCurrentSense currentSense = LowsideCurrentSense(0.003, -64.0/7.0, A_OP1_O
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 
 
-
-
- 
-
-#define MONITOR true
+#define MONITOR false
 #define DO_CALIBRATION false
 
 enum WhichMotor {
@@ -23,112 +17,14 @@ enum WhichMotor {
   RIGHT_MOTOR
 };
 
-
 // CHANGE THIS DEPENDING ON WHICH MOTOR YOU ARE FLASHING
 enum WhichMotor this_motor = RIGHT_MOTOR;
 
 
-const long SMOOTHING_WINDOW_SIZE = 10;
-long input_window[SMOOTHING_WINDOW_SIZE] = {0};
-int index_num = 0;
-long sum = 0;
-
-long smooth_input(long new_value) {
-  index_num = (index_num + 1) % SMOOTHING_WINDOW_SIZE;
-  sum -= input_window[index_num];
-  input_window[index_num] = new_value;
-  sum += new_value;
-  return sum / SMOOTHING_WINDOW_SIZE;
-}
-
-
-
-// Values from PCA as tuned by the servo tester
-#define PULSE_LOW_END 1087
-#define PULSE_HIGH_END 1860
-
 #define TOP_SPEED 150.0
-
-// volatile long prev_time = 0;
-// volatile long current_time = 0;
-// volatile long disconnect_counter = 0;
-volatile long time_between_pulses_micro_seconds = 0;
-
 float throttle = 0.0;
 
 
-
-
-// uint32_t custom_timer() {
-//   // timer->pause();
-//   uint32_t res = timer->getCount(MICROSEC_FORMAT);
-//   // timer->resume();
-//   // timer->setCount(0);
-//   // return micros();
-//   return res;
-
-// }
-// void signal_change() {
-//   disconnect_counter = 0;
-//   int current_state = digitalRead(A_PWM);
-//   if (current_state == HIGH) {
-//     prev_time = custom_timer();
-//     return;
-//   }
-//   current_time = custom_timer();
-//   delta = current_time - prev_time;
-//   // if (delta > 0 ) {
-//   //   time_between_pulses_micro_seconds = smooth_input(delta);
-//   // }
-// }
-
-
-
-// #define DEAD_ZONE_THRESH 1.0f
-
-// void pwm_signal_read() {
-//   if (disconnect_counter > 200) {
-//     throttle = 0.0f;
-//     return;
-//   }
-
-//   if (time_between_pulses_micro_seconds <= PULSE_HIGH_END && time_between_pulses_micro_seconds >= PULSE_LOW_END) {
-//     throttle = (((time_between_pulses_micro_seconds - PULSE_LOW_END) / (float) (PULSE_HIGH_END - PULSE_LOW_END)) * 2.0f * TOP_SPEED) - TOP_SPEED;
-
-//     if (throttle > TOP_SPEED) { throttle = TOP_SPEED; }
-//     if (throttle < -TOP_SPEED) { throttle = -TOP_SPEED; }
-
-//     // For now no dead_zone
-//     // if ((throttle <= DEAD_ZONE_THRESH) && (throttle >= 0.0f - DEAD_ZONE_THRESH)) {
-//     //   throttle = 0.0;
-//     // }
-//   }
-//   disconnect_counter += 1;
-// }
-
-
-// void pwm_signal_init() {
-  // pinMode(A_PWM, INPUT);
-  // timer->setMode(0, TIMER_INPUT_CAPTURE_BOTHEDGE, A_PWM);
-  // timer->setOverflow(10000, MICROSEC_FORMAT);
-  // timer->resume();
-
-  // USE THIS 
-  // https://github.com/stm32duino/STM32Examples/blob/main/examples/Peripherals/HardwareTimer/Frequency_Dutycycle_measurement/Frequency_Dutycycle_measurement.ino
-  
-  
-  
-  // attachInterrupt(digitalPinToInterrupt(A_PWM), signal_change, CHANGE);
-  
-  // digitalPinToInterrupt(A_PWM) 
-
-
-  // todo: attach interrupt to timer2
-  // todo: set time_between_pulses_micro_seconds to be pulses
-
-// }
-
-// This makes the motors warm, removing plastic from Motor shaft would make this ok.
 #define MAX_VOLTAGE 2.7
 #define MAX_CURRENT 7.0
 
@@ -150,10 +46,6 @@ void setup() {
     Serial.println("--------------");
   }
 
-  // INIT FOR READING PWM
-  
-
-
   // MAGNETIC SENSOR INIT
   sensor.init();
   motor.linkSensor(&sensor);
@@ -163,7 +55,6 @@ void setup() {
   driver.voltage_power_supply = 16;
   driver.init();
   motor.linkDriver(&driver);
-
 
   // MOTOR INIT
   motor.current_limit = MAX_CURRENT;
@@ -189,7 +80,6 @@ void setup() {
     }
   }
 
-
   // CURRENT SENSOR
   currentSense.linkDriver(&driver);
   currentSense.init();
@@ -206,15 +96,20 @@ void setup() {
 
 
 void loop() {
+  if (adjusted_duty < 100 || adjusted_duty > 200) {
+    throttle = 0.0;
+  } else {
+    throttle = ((float) (adjusted_duty-100) / 100.0) * (TOP_SPEED * 2) - TOP_SPEED;
+  }
+
   motor.loopFOC();
   motor.move(throttle);
 
   if (MONITOR) {
-    // Serial.print(frequency_measured);
-    // Serial.print("    ");
-    // Serial.print(dutycycle_measured);
-    // Serial.println("    ");
-    // Serial.println(throttle);
+    Serial.print(adjusted_duty);
+    Serial.print("    ");
+    Serial.print(throttle);
+    Serial.println("    ");
   }
 }
 
